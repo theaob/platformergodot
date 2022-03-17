ec222818c845b105bd64740c1b423048d43aa070
Onur Baykal <theaob@gmail.com>
2022-03-17 22:09:54 +0300

    added animation added h flip for direction

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -9,14 +9,23 @@ const LERP_WEIGHT = 0.15;
 func _physics_process(delta):
 	if Input.is_action_pressed("right"):
 		velocity.x = X_SPEED;
-	if Input.is_action_pressed("left"):
+		$AnimatedSprite.play("walk");
+		$AnimatedSprite.flip_h = false;
+	elif Input.is_action_pressed("left"):
 		velocity.x = -X_SPEED;
+		$AnimatedSprite.play("walk");
+		$AnimatedSprite.flip_h = true;
+	else:
+		$AnimatedSprite.play("idle");
+		
+	if not is_on_floor():
+		$AnimatedSprite.play("jump")
 
 	velocity.y = velocity.y + GRAVITY;
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_FORCE;
-	
+		
 	velocity = move_and_slide(velocity, Vector2.UP);
 
 	velocity.x = lerp(velocity.x, 0, LERP_WEIGHT);

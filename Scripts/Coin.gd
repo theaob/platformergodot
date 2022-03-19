941afd8677eebae0238776b81ad993f4b0b3c536
Onur Baykal <theaob@gmail.com>
2022-03-19 10:22:35 +0300

    added coin collect animation

--- Scripts/Coin.gd
+++ Scripts/Coin.gd
@@ -1,4 +1,7 @@
 extends Area2D
 
-func _on_coin_body_entered(body):
+func _on_coin_body_entered(_body):
+	$AnimationPlayer.play("coin_animation");
+
+func _on_AnimationPlayer_animation_finished(_anim_name):
 	queue_free();
